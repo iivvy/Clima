a4c0aae76fe360d1c71e2c6e37554d66e21568cb
TheMuellenator <themuellenator@users.noreply.github.com>
2020-01-17 19:28:50 +0000

    Removing android.support dependencies Updating gradle wrapper to 5.4.1-all.zip Updating gradle build to 3.5.3

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
+            getLocation();
             //Get the current location
           },
           child: Text('Get Location'),
